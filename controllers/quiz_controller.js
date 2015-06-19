7469a6b658898d9427e2be333f8404459bc4919d
Óscar Tenorio <oscar.tenorio@gmail.com>
2015-06-19 11:24:34 +0200

    Modelo y base de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,20 @@
+var models = require("../models/models.js");
+
 // GET /quizes/question
 exports.question = function(req, res) {
-  res.render('quizes/question', {title: 'Quiz - Pregunta',pregunta: 'Capital de Italia'});
+  models.Quiz.findAll().then(function(quiz) {
+      res.render('quizes/question', {pregunta: quiz[0].pregunta});
+  })
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-  if (req.query.respuesta.toLowerCase() === 'roma') {
-    res.render('quizes/answer', {title: 'Quiz - Respuesta', respuesta: 'Correcto'});
-  }
-  else {
-    res.render('quizes/answer', {title: 'Quiz - Respuesta',respuesta: 'Incorrecto'});
-  }
+    models.Quiz.findAll().then(function(quiz) {
+        if (req.query.respuesta.toLowerCase() === quiz[0].respuesta.toLowerCase()) {
+            res.render('quizes/answer', {respuesta: 'Correcto'});
+        }
+        else {
+            res.render('quizes/answer', {respuesta: 'Incorrecto'});
+        }
+    });
 };
