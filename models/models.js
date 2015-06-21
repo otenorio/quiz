2bc51f70a71b241d6d15fe48c27dfc7c3c3d047f
Óscar Tenorio <oscar.tenorio@gmail.com>
2015-06-21 23:28:30 +0200

    Lista de Preguntas

--- models/models.js
+++ models/models.js
@@ -31,12 +31,13 @@ exports.Quiz = Quiz; // exportar definición de la tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 sequelize.sync().then(function() {
-  // success(...) ejecuta el manejador una vez creada la tabla
+  // then(...) ejecuta el manejador una vez creada la tabla
   Quiz.count().then(function (count) {
     if(count === 0) { // la tabla se inicializa solo si está vacía
-      Quiz.create( { pregunta: "Capital de Italia",
-                                respuesta: "Roma"
-                            }).then(function() {console.log("Base de datos inicializada")});
+      Quiz.bulkCreate(
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'}
+          ]).then(function() {console.log("Base de datos inicializada")});
     };
   });
 });
