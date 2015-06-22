6e4b33012178e4ff80d66e44440c8ec4ec2a3a5d
Óscar Tenorio <oscar.tenorio@gmail.com>
2015-06-22 20:36:43 +0200

    Editar Pregunta

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,9 @@ router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new', quizController.new);
 router.post('/quizes/create', quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.get('/author', function(req, res) { res.render('author', {}); });
 router.get('/search', function(req, res) { res.render('search', {}); });
+router.put('/quizes/:quizId(\\d+)', quizController.update);
 
 module.exports = router;
