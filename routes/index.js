e75443347210aa4f3cafe77c0eb4b03a8797035e
Óscar Tenorio <oscar.tenorio@gmail.com>
2015-06-23 11:24:17 +0200

    Comentarios

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
+var commentController = require('../controllers/comment_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -22,5 +23,8 @@ router.get('/author', function(req, res) { res.render('author', {}); });
 router.get('/search', function(req, res) { res.render('search', {}); });
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
 module.exports = router;
