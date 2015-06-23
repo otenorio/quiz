13da4be00f9a6d0fc064b5a5595ccfcf8253033b
Óscar Tenorio <oscar.tenorio@gmail.com>
2015-06-23 09:24:57 +0200

    Indice Temático

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,7 +27,7 @@ exports.index = function(req, res) {
 // GET /quizes/new
 exports.new = function(req, res) {
  var quiz = models.Quiz.build(
-     {pregunta: "Pregunta", respuesta: "Respuesta"});
+     {pregunta: "", respuesta: "", tema: "Otros"});
       res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
@@ -57,7 +57,7 @@ exports.create = function(req, res) {
       res.render('quizes/new', {quiz: quiz, errors: err.errors});
     } else {
       // Guarda en BD los campos pregunta y respuesta de quiz
-       quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+       quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function() {
            res.redirect('/quizes');
        }); // Redirección HTTP a lista de preguntas
     }
@@ -79,7 +79,7 @@ exports.update = function(req, res) {
       res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
     } else {
       // Guarda en BD los campos pregunta y respuesta de quiz
-       req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
+       req.quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function() {
            res.redirect('/quizes');
        }); // Redirección HTTP a lista de preguntas
     }
--- models/quiz.js
+++ models/quiz.js
@@ -5,11 +5,14 @@ module.exports = function(sequelize, DataTypes) {
     { pregunta: {
         type: DataTypes.STRING,
         validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-      },
+        },
       respuesta: {
         type: DataTypes.STRING,
         validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-      }
-    }
-  );
+        },
+      tema: {
+        type: DataTypes.STRING,
+        validate: {}
+        }
+    });
 }
